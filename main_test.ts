4cb073a1b14313967e6e7baca62dd9d0d79b17a2
ykisii <ishiiyuuki@gmail.com>
2023-07-10 18:02:46 +0900

    add: implements, read sheet

--- main_test.ts
+++ main_test.ts
@@ -1,13 +1,22 @@
 import { assertEquals, assertExists, assertFalse } from "https://deno.land/std@0.192.0/testing/asserts.ts";
 import { XlsxToMD } from "./main.ts";
 
-Deno.test("disp data", async () => {
-  await XlsxToMD.convert("sample.xlsx", true).then((data)=> {
-    console.log(data);
+Deno.test("file opened", async () => {
+  await XlsxToMD.convert({filePath:"sample.xlsx", sheetNumber:0}).then((data)=> {
+    //console.log(data);
     assertFalse(false);
   }).catch((reason) => {
-    console.log(reason);
+    console.log(reason.name);
     assertFalse(true);
   });
 });
 
+Deno.test("path is empty", async () => {
+  await XlsxToMD.convert({filePath:""}).then((data)=> {
+    //console.log(data);
+    assertFalse(true);
+  }).catch((reason) => {
+    console.log(reason.name);
+    assertFalse(false);
+  });
+});
--- src/xlsx_to_md.ts
+++ src/xlsx_to_md.ts
@@ -1,16 +1,36 @@
 import * as XLSX from "https://cdn.sheetjs.com/xlsx-0.18.3/package/xlsx.mjs";
 
+type SourceInfo = {
+  filePath: string;
+  sheetNumber?: number | undefined;
+  hederExists?: boolean | undefined; 
+};
+
 export class XlsxToMD {
-  
-  static convert(filePath: string, existHeader: boolean = false) {
+  static convert(info: SourceInfo) {
     return new Promise((resolve, reject) => {
       try {
-        const u8: Uint8Array = Deno.readFileSync(filePath);
+        const u8: Uint8Array = Deno.readFileSync(info.filePath);
         const wb = XLSX.read(u8);
+        this.getMDData(wb, info);
         resolve(wb);
-      } catch {
-        reject("can not open file.");
+      } catch(e) {
+        reject(e);
       }
     });
+  }
+  private static getMDData(wb:any, info: SourceInfo) {
+    if (wb == null || info == null) throw new Error("parameter is null/undefined");
+    const sheet = wb.Sheets[wb.SheetNames[info.sheetNumber ?? 0]];
+    const header = info.hederExists ?? false;
+    const range = XLSX.utils.decode_range(sheet['!ref']);
+
+    for (let row = range.s.r; row <= range.e.r; row++) {
+      for (let col = range.s.c; col <= range.e.c; col++) {
+        const pos = XLSX.utils.encode_cell({r:row, c:col});
+        const cell = sheet[pos];
+        console.log(cell.v);
+      }
+    }
   }
 }
